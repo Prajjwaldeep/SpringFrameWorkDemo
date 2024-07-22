f097ccd77c5cd5fed3e0173083a23e729912247c
Prajjwaldeep Ghosh <prajjwaldeepghosh.leo@gmail.com>
2024-07-22 21:22:30 +0530

    Dependency Injection - Constructor Injection

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,8 +1,16 @@
 package demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor doctor=new Doctor();
-        doctor.assist();
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+
+        Doctor staff=context.getBean(Doctor.class);
+        staff.assist();
+        System.out.println(staff.getQualification());
     }
 }
